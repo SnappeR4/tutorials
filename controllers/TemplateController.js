0f9dfc65fe2f13e4090745a3a2b28224c5e50949
SnappeR4 <rahulwaghmare751@gmail.com>
2025-02-19 16:40:49 +0530

    Done with the add and delete template

--- controllers/TemplateController.js
+++ controllers/TemplateController.js
@@ -1,93 +1,98 @@
-const { error } = require('console')
-const Template = require('../models/Template')
+const Template = require('../models/Template');
 
-// Show the list of Templates
-const index = (req, res, next) => {
-    Template.find()
-    .then(response => {
-        res.json({
-            response
-        })
-    })
-    .catch(error => {
-        res.json({
-            message: 'An error Occured!'
-        })
-    })
-}
-// get single template
-const show = (req, res, next) => {
-    let templateID = req.body.templateID
-    Template.findById(templateID)
-    .then(response => {
-        res.json({
-            response
-        })
-    })
-    .catch(error => {
-        res.json({
-            message: 'An error Occured!'
-        })
-    })
-}
-
-// add new template
-const store = (req, res, next) => {
-    let template = new Template({
-        name: req.body.name,
-        templateCategory: req.body.templateCategory,
-        jsonData: req.body.jsonData
-    })
-    template.save()
-    .then(response => {
-        res.json({
-            message: 'Template Added Successfully!'
-        })
-    })
-    .catch(error => {
-        res.json({
-            message: 'An error Occured!'
-        })
-    })
-}
-
-// update template
-const update = (req, res, next) => {
-    let templateID = req.body.templateID
-    let updateData = {
-        name: req.body.name,
-        templateCategory: req.body.templateCategory,
-        jsonData: req.body.jsonData
+// Show all templates
+const index = async (req, res) => {
+    try {
+        const templates = await Template.find();
+        res.status(200).json({
+            success: true,
+            data: templates,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'An error occurred while fetching templates.',
+        });
     }
-    Template.findByIdAndUpdate(templateID, {$set: updateData})
-    .then(response => {
-        res.json({
-            message: 'Template Updated Successfully!'
-        })
-    })
-    .catch(error => {
-        res.json({
-            message: 'An error Occured!'
-        })
-    })
-}
+};
 
-// delete template
-const destroy = (req, res, next) => {
-    let templateID = req.body.templateID
-    Template.findByIdAndDelete(templateID)
-    .then(response => {
-        res.json({
-            message: 'Template Deleted Successfully!'
-        })
-    })
-    .catch(error => {
-        res.json({
-            message: 'An error Occured!'
-        })
-    })
-}
+// Show a single template
+const show = async (req, res) => {
+    const { templateID } = req.body;
+    if (!templateID) {
+        return res.status(400).json({ success: false, message: 'templateID is required.' });
+    }
 
-module.exports = {
-    index, show, store, update, destroy
-}
+    try {
+        const template = await Template.findById(templateID);
+        if (!template) {
+            return res.status(404).json({ success: false, message: 'Template not found.' });
+        }
+        res.status(200).json({ success: true, data: template });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'An error occurred while fetching the template.' });
+    }
+};
+
+// Add new template
+const store = async (req, res) => {
+    const { name, templateCategory, jsonData } = req.body;
+
+    if (!name || !templateCategory || !jsonData) {
+        return res.status(400).json({ success: false, message: 'All fields are required.' });
+    }
+
+    try {
+        const newTemplate = new Template({ name, templateCategory, jsonData });
+        await newTemplate.save();
+        res.status(201).json({ success: true, message: 'Template added successfully.' });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'An error occurred while adding the template.' });
+    }
+};
+
+// Update existing template
+const update = async (req, res) => {
+    const { templateID, name, templateCategory, jsonData } = req.body;
+
+    if (!templateID || !name || !templateCategory || !jsonData) {
+        return res.status(400).json({ success: false, message: 'All fields are required.' });
+    }
+
+    try {
+        const updatedTemplate = await Template.findByIdAndUpdate(
+            templateID,
+            { name, templateCategory, jsonData },
+            { new: true } // Returns the updated document
+        );
+
+        if (!updatedTemplate) {
+            return res.status(404).json({ success: false, message: 'Template not found.' });
+        }
+
+        res.status(200).json({ success: true, message: 'Template updated successfully.', data: updatedTemplate });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'An error occurred while updating the template.' });
+    }
+};
+
+// Delete template
+const destroy = async (req, res) => {
+    const { templateID } = req.body;
+
+    if (!templateID) {
+        return res.status(400).json({ success: false, message: 'templateID is required.' });
+    }
+
+    try {
+        const deletedTemplate = await Template.findByIdAndDelete(templateID);
+        if (!deletedTemplate) {
+            return res.status(404).json({ success: false, message: 'Template not found.' });
+        }
+        res.status(200).json({ success: true, message: 'Template deleted successfully.' });
+    } catch (error) {
+        res.status(500).json({ success: false, message: 'An error occurred while deleting the template.' });
+    }
+};
+
+module.exports = { index, show, store, update, destroy };
